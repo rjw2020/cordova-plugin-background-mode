06f2a7cad4aab6e83d7e3392874316a572591ccf
YimasoftLiuGuanJun <35481331+YimasoftLiuGuanJun@users.noreply.github.com>
2018-11-15 20:00:20 +0800

    Update VvHuaWeiPushTestReceiver.java

--- src/android/com/huawei/android/hms/agent/common/VvHuaWeiPushTestReceiver.java
+++ src/android/com/huawei/android/hms/agent/common/VvHuaWeiPushTestReceiver.java
@@ -4,6 +4,7 @@ import android.app.NotificationManager;
 import android.content.Context;
 import android.os.Bundle;
 import com.huawei.hms.support.api.push.PushReceiver;
+import de.appplant.cordova.plugin.background.VVServer;
 
 //通过广播接收Push连接状态
 public class VvHuaWeiPushTestReceiver extends PushReceiver {
@@ -15,7 +16,7 @@ public class VvHuaWeiPushTestReceiver extends PushReceiver {
         try {
             //CP可以自己解析消息内容，然后做相应的处理
             String content = new String(msgBytes, "UTF-8");
-            //AsyncLogger.Logging(TAG, "收到PUSH透传消息,消息内容为:" + content);
+            VVServer.WriteLog(context, "收到PUSH透传消息,消息内容为:" + content);
         } catch (Exception e) {
             e.printStackTrace();
         }
@@ -26,14 +27,13 @@ public class VvHuaWeiPushTestReceiver extends PushReceiver {
 
     @Override
     public void onPushState(Context context, boolean pushState) {
-        //AsyncLogger.Logging(TAG, "onPushState:" + pushState);
+        VVServer.WriteLog(context, "onPushState:" + pushState);
         super.onPushState(context, pushState);
     }
 
     @Override
     public void onToken(Context context, String token) {
-        str_append = token;
-        //AsyncLogger.Logging(TAG, "onToken:" + token);
+        VVServer.WriteLog(context, "onToken:" + token);
         super.onToken(context, token);
     }
 
@@ -41,7 +41,7 @@ public class VvHuaWeiPushTestReceiver extends PushReceiver {
     public void onEvent(Context context, Event event, Bundle extras) {
         if (Event.NOTIFICATION_OPENED.equals(event) || Event.NOTIFICATION_CLICK_BTN.equals(event)) {
             int notifyId = extras.getInt(BOUND_KEY.pushNotifyId, 0);
-            //AsyncLogger.Logging(TAG, "收到通知栏消息点击事件,notifyId:" + notifyId);
+            VVServer.WriteLog(context, "收到通知栏消息点击事件,notifyId:" + notifyId);
             if (0 != notifyId) {
                 NotificationManager manager = (NotificationManager) context.getSystemService(Context.NOTIFICATION_SERVICE);
                 if (manager != null) {
